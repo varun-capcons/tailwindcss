dc459591e0b705a7cd00025d060cea2a454d581f
varun chandra <varun@capcons.com>
2022-08-24 13:56:48 +0530

    varun chandra

---
+++ pages/colors.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+const colors = () => {
+  return (
+    <div>
+      {/* default colors
+        white, black, red, green, blue, orange, yellow, purple, lime, emerald, teal, cyan, indigo, violet, fuchsia, pink, rose, sky, gray, slate, zinc, neutral, stone, amber,
+     */}
+      {/* Text Colors */}
+      <p>
+        tailwind is a css framework that uses low-level utility classes to
+        create layout
+      </p>
+      <p>Other utility classes framework includes Tachyons and Windi CSS</p>
+      <p>
+        Utility classes operates at low-level. This means we have more control
+        and flexibility over design
+      </p>
+      <p>Utility classes can be adopted to any design</p>
+      <p>Utility classes can be completely customizable and extensible</p>
+      <p>
+        Utility classes allows for much easier implementation of responsive
+        design patterns
+      </p>
+      <p>
+        With Utility classes, we can extract common, repetitive patterns intom
+        custom, reusable components
+      </p>
+      <p>Utility classes gives us balance between the concrete and abstract</p>
+      <p>Utility classes are easy to customize, so we can build any design </p>
+      <p>Named according to their purpose.</p>
+      {/* Background Colors
+       */}
+      <h2>Difference between Bootstrap and TailwindCSS</h2>
+      <h3>tailwind CSS</h3>
+      <p>Newer Framework that is gaining popularity</p>
+      <p>Low-level classes for fast UI design</p>
+      <p>More flexiblility and Uniqueness </p>
+      <p>Customizable with directive and functions</p>
+      <p>Tailwind has a lot of classes</p>
+      <h3>Bootstrap</h3>
+      <p>Popular framework that has been around for a while</p>
+      <p>High level pre-designed Components</p>
+      <p>Some Bootstrap websites look very similar</p>
+      <p>Customizable through SaSS</p>
+      <p>Easy for beginners</p>
+      {/* Text Underline */}
+      <h2>There are four main factros involved in every layout</h2>
+      <h3>Layout</h3>
+      <p>
+        This defines how the whitespace and elements of our design are organized
+        and ordered
+      </p>
+      <h3>Typography</h3>
+      <p>This includes all text content, including messages</p>
+      <h3>Colors</h3>
+      <p>This brings life to a design and defines a design mood and brand</p>
+      <h3>Imagery</h3>
+      <p>
+        This includes the visuals of a design, such as icons, images and
+        illustrations
+      </p>
+
+      {/* Border Colors */}
+
+      {/* Divide Colors */}
+
+      {/* <!-- Outline Colors --> */}
+
+      {/* <!-- Box Shadow Colors (Opacity defaults to 100, but you can set it)--> */}
+
+      {/* Accent Colors */}
+
+      {/* Arbitrary Colors */}
+    </div>
+  );
+};
+
+export default colors;
